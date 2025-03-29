c2e220f1c5ec73628497c47099edca10383067fa
anet-eny <anet-eny@gmail.com>
2025-03-29 13:42:36 +0100

    Create Guess component and set the logic rendering cells with player's guesses

--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+
+import Guess from '../Guess';
+
 function GuessList({guesses}) {
 
   return <div className="guess-results">
-            {guesses.map((guess, index) => (
-              <p key={index} className="guess">{guess}</p>
+            {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+              <Guess key={num} value={guesses[num]}/>
             ))}
             
-        </div>;
+          </div>;
 }
 
 export default GuessList;
