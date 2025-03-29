58dff9c7358d7f44c2ee1830f42ae0e8b9149067
anet-eny <anet-eny@gmail.com>
2025-03-29 16:01:42 +0100

    Create GameOverBanner component and pass the gameStatus state to render correct banner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+
 import GuessInput from '../GuessInput';
 import GuessList from '../GuessList';
+import GameOverBanner from '../GameOverBanner';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,19 +14,29 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [gameStatus, setGameStatus] = React.useState('running')
   const [guesses, setGuesses] = React.useState([])
 
   function handleSubmitGuess(guess) {
-    setGuesses([...guesses, guess])
+    const nextGuesses = [...guesses, guess]
+    setGuesses(nextGuesses)
+
+    if (guess.toUpperCase() === answer) {
+      setGameStatus('won')
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lost')
+    }
   }
   
   return <>
+          {gameStatus}
           <GuessList
             guesses={guesses}
             answer={answer}
           />
           <GuessInput 
             handleSubmitGuess={handleSubmitGuess}
+            gameStatus={gameStatus}
           />
         </>;
 }
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function GuessInput({handleSubmitGuess}) {
+function GuessInput({handleSubmitGuess, gameStatus}) {
   const [guess, setGuess] = React.useState('')
 
   function handleSubmit(event) {
@@ -21,6 +21,7 @@ function GuessInput({handleSubmitGuess}) {
               </label>
               <input 
                 required
+                disabled={gameStatus !== 'running'}
                 minLength={5}
                 maxLength={5}
                 pattern="[a-zA-Z]{5}"
